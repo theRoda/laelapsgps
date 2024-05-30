0c29a76f625018052288480c090b10057dd98a6d
theRoda <thisreallylongname@gmail.com>
2024-05-30 09:09:04 -0400

    fix sd card

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import sdcard, os
 from time import sleep
 from machine import UART
-from machine import SPI
+#from machine import SPI
+from machine import SoftSPI
 from machine import Pin
 import uping
 
 connectWifi()
 uping.ping('8.8.8.8')
-spi = SPI(1, baudrate=20000000) # SPI interface on ESP32, SCK and MOSI pins are 14 and 15 respectively
-cs = Pin(13) # CS pin is 13
+
+# MISO GPIO13
+# MOSI GPIO12
+# SCK GPIO14
+# CS GPIO27
+# GND - GND on left, next to VIN
+# VCC - VIN, on left
+spi = SoftSPI(-1, miso=Pin(13), mosi=Pin(12), sck=Pin(14))
+cs = Pin(27)
+
+
 sd = sdcard.SDCard(spi, cs)
 
 vfs = os.VfsFat(sd)
@@ -20,6 +30,11 @@ with open("/sd/test.txt", "w") as f:
 with open("/sd/test.txt", "r") as f:
     print(f.read())
 
+sddir = os.listdir("/sd")
+print(f'/sd {sddir}')
+
+# GREEN - ESP32 RX2 -> NEO-6M TX2
+# BLUE - ESP32 TX2 -> NEO-6M RX2
 gps = UART(2, baudrate=9600)
 print(gps)
 

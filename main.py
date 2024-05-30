d6e17b7677be7ddaa6d60adf9b2a8e5b6a931f20
theRoda <thisreallylongname@gmail.com>
2024-05-30 09:35:22 -0400

    fix decode issue

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sdcard, os
 from time import sleep
 from machine import UART
-#from machine import SPI
+# from machine import SPI
 from machine import SoftSPI
 from machine import Pin
 import uping
@@ -17,21 +17,10 @@ uping.ping('8.8.8.8')
 # VCC - VIN, on left
 spi = SoftSPI(-1, miso=Pin(13), mosi=Pin(12), sck=Pin(14))
 cs = Pin(27)
-
-
 sd = sdcard.SDCard(spi, cs)
 
 vfs = os.VfsFat(sd)
 os.mount(sd, "/sd")
-
-with open("/sd/test.txt", "w") as f:
-    f.write("Hello, World!")
-
-with open("/sd/test.txt", "r") as f:
-    print(f.read())
-
-sddir = os.listdir("/sd")
-print(f'/sd {sddir}')
 
 # GREEN - ESP32 RX2 -> NEO-6M TX2
 # BLUE - ESP32 TX2 -> NEO-6M RX2
@@ -43,10 +32,14 @@ while True:
     line = gps.readline()
     if line is not None:
         try:
-            decoded = line.decode().rstrip()
-            chunks = decoded.split(',')
-            if chunks[0] == '$GPGGA':
-                print(decoded)
+            decoded = line.decode('utf-16')
+            with open("/sd/gps_sentences.txt", "a") as f:
+                f.write(decoded)
+            print(decoded)
+            # chunks = decoded.split(',')
+            # if chunks[0] == '$GPGGA':
+            #    print(decoded)
         except UnicodeError:
             pass
+
 
